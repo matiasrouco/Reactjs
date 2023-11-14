704e6f1e8efce799d822c0cf81abc7b813517e27
Matias Rouco <mrouco@emerix.net>
2023-11-14 20:37:47 -0300

    agregado de productos y promise y maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Maps } from "./components";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { NavBar } from "./components/navBar/NavBar";
 
 export const App = () => {
 	return (
 		<>
+			
 			<NavBar />
 			<ItemListContainer message="Lista de Articulos a Vender"/>
+			<Maps />
 		</>
 	);
 };
